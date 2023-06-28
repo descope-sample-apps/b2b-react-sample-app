bd7ab32ee986921994dc01acf0f050fa0372daa9
Allen Zhou <46854522+allenzhou101@users.noreply.github.com>
2023-06-28 14:54:30 -0500

    Update programmatic_login_spec.cy.js

--- cypress/e2e/programmatic_login_spec.cy.js
+++ cypress/e2e/programmatic_login_spec.cy.js
@@ -6,15 +6,9 @@ describe('Descope', function () {
   })
 
   it('shows welcome page', function () {
-    /* ==== Generated with Cypress Studio ==== */
     cy.get('.ant-avatar-string').click();
     cy.get('.ant-popover-inner').contains('Hey');
-    /* ==== End Cypress Studio ==== */
-    /* ==== Generated with Cypress Studio ==== */
     cy.get('a > p').click();
-    // cy.get('descope-wc').click();
     cy.contains('Sign in').should('be.visible')
-
-    /* ==== End Cypress Studio ==== */
   })
 })
