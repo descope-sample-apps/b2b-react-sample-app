3fd27a2770c6c57af3fd802ce9751efffc31ee64
Chris Carper <chris@descope.com>
2023-06-30 12:32:08 -0500

    Fixed admin page and updated deps

--- src/dashboard/dataTables/DataTables.js
+++ src/dashboard/dataTables/DataTables.js
@@ -1,4 +1,4 @@
-import { Col, Row, Space } from "antd";
+import { Box, SimpleGrid, useColorMode } from "@chakra-ui/react";
 import React, { useState } from "react";
 import DevelopmentTables from "./component/DevelopmentTables";
 import {
@@ -13,107 +13,92 @@ import ColumnsTable from "./component/ColumnsTable";
 import ComplexTable from "./component/ComplexTable";
 import AdminExperiences from "../../components/adminExperiences/AdminExperiences";
 import { getSessionToken, Descope } from '@descope/react-sdk';
-import { useNavigate } from "react-router-dom";
 
+// export default DataTables;
+export default function DataTables() {
+	const [data, setData] = useState({
+		check: [],
+		columns: [],
+		development: [],
+		complex: [],
+		loaded: false,
+	});
+	const { colorMode } = useColorMode();
 
-const DataTables = () => {
-  const [data, setData] = useState({
-    check: [],
-    columns: [],
-    development: [],
-    complex: [],
-    loaded: false,
-  });
-  const [authenticationFlow, setAuthenticationFlow] = useState(false);
-  const navigate = useNavigate();
+	const [authenticationFlow, setAuthenticationFlow] = useState(false);
+	const projectId = localStorage.getItem('projectId') || process.env.REACT_APP_DESCOPE_PROJECT_ID;
+	const sessionToken = getSessionToken();
 
-  const projectId = localStorage.getItem('projectId') || process.env.REACT_APP_DESCOPE_PROJECT_ID;
-  const sessionToken = getSessionToken();
+	if (!data.loaded) {
+		fetch("/api/data", {
+			method: "get",
+			headers: {
+				Accept: "application/json, text/plain, */*",
+				"Content-Type": "application/json",
+				"x-project-id": projectId,
+				Authorization: `Bearer ${sessionToken}`,
+			},
+		})
+		.then((response) => {
+			if (response.status === 401) {
+				setAuthenticationFlow(true);
+			}  else {
+				setAuthenticationFlow(false);
+				return  response.json();
+			}
+		})
+		.then((res) => {
+			if (res) {
+				res.body.loaded = true;
+				setData(res.body);
+				setAuthenticationFlow(false);
+			}
+		})
+		.catch((err) => console.log('err => ', err));
+	}
 
-  if (!data.loaded) {
-    fetch("/api/data", {
-      method: "get",
-      headers: {
-        Accept: "application/json, text/plain, */*",
-        "Content-Type": "application/json",
-        "x-project-id": projectId,
-        Authorization: `Bearer ${sessionToken}`,
-      },
-    })
-      .then((response) => {
-        if (response.status === 401 || response.status === 404) {
-          setAuthenticationFlow(true);
-        } else {
-          setAuthenticationFlow(false);
-          return response.json();
+	return (
+		<Box pt={{ base: "130px", md: "80px", xl: "80px" }}>
+			<SimpleGrid
+				mb="20px"
+				columns={authenticationFlow ? { sm: 1, md: 1 }: { sm: 1, md: 2 }}
+				spacing={{ base: "20px", xl: "20px" }}
+			>
+        { authenticationFlow &&
+            (
+              <Box margin={'auto'} width='50%'>
+                <Descope
+                flowId="step-up"
+                onSuccess={(e) => {
+                  console.log('success => ', e)
+                }}
+                onError={(e) => console.log("Error!")}
+                theme={ colorMode }
+                />
+              </Box>
+            )
         }
-
-      })
-      .then((res) => {
-        if (res) {
-          res.body.loaded = true;
-          setData(res.body);
-          setAuthenticationFlow(false);
-        }
-      })
-      .catch((err) => console.log('err => ', err));
-
-  }
-  return (
-    <div className="data-table-wrapper">
-      {
-        authenticationFlow ?
-          <div style={{ margin: 'auto', maxWidth: '450px', borderRadius: "10px", overflow: "hidden", width: "100%" }}>
-            <Descope
-              flowId="step-up"
-              onSuccess={(e) => {
-                console.log('success => ', e)
-                navigate("admin/data-tables");
-
-              }}
-              onError={(e) => console.log("Error!")}
-            />
-          </div>
-          :
-          <>
-            <Space size="large" className="first-row">
-              <Row gutter={[14, 14]}>
-                <Col sm={24} md={12} lg={12}>
-                  <DevelopmentTables
-                    columnsDataDevelopment={columnsDataDevelopment}
-                    tableDataDevlopment={data.development}
-                  />
-                </Col>
-                <Col sm={24} md={12} lg={12}>
-                  <CheckTable
-                    columnsDataCheck={columnsDataCheck}
-                    tableDataCheck={data.check}
-                  />
-                </Col>
-              </Row>
-            </Space>
-            <Space size="large" className="sec-row">
-              <Row gutter={[14, 14]}>
-                <Col sm={24} md={12} lg={12}>
-                  <ColumnsTable
-                    columnsDataColumns={columnsDataColumns}
-                    tableDataColumns={data.columns}
-                  />
-                </Col>
-                <Col sm={24} md={12} lg={12}>
-                  <ComplexTable
-                    columnsDataComplex={columnsDataComplex}
-                    tableDataComplex={data.complex}
-                  />
-                </Col>
-              </Row>
-            </Space>
-          </>
-      }
-      <br />
-      <AdminExperiences />
-    </div>
-  );
-};
-
-export default DataTables;
+				{
+					<>
+						<DevelopmentTables
+							columnsData={columnsDataDevelopment}
+							tableData={data.development}
+						/>
+						<CheckTable columnsData={columnsDataCheck} tableData={data.check} />
+						<ColumnsTable
+							columnsData={columnsDataColumns}
+							tableData={data.columns}
+						/>
+						<ComplexTable
+							columnsData={columnsDataComplex}
+							tableData={data.complex}
+						/>
+					</>
+				}
+			</SimpleGrid>
+			<Box display={'flex'} justifyContent={'center'}>
+					<AdminExperiences/>
+			</Box>
+		</Box>
+	);
+}
