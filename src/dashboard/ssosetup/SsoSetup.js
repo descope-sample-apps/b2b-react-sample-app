9690450bb82b12893fc78b25c8311d687b58817f
Chris Carper <42780533+chris4490@users.noreply.github.com>
2023-08-09 15:27:36 -0500

    Fix identify error and navigate once SAML config is done. (#81)
    * fix-indity-error-and-navigate-when-saml-config-done
    
    * Fixed navigate
    
    * reverted small changes in sso setup

--- src/dashboard/ssosetup/SsoSetup.js
+++ src/dashboard/ssosetup/SsoSetup.js
@@ -1,9 +1,12 @@
-import React from "react";
+import React, { Fragment } from "react";
 import "./ssoSetup.scss";
-// import AdminExperiences from "../../components/adminExperiences/AdminExperiences";
-import { Descope } from '@descope/react-sdk';
+import AdminExperiences from "../../components/adminExperiences/AdminExperiences";
+import { Descope, useSession, useUser, getJwtPermissions, getSessionToken } from '@descope/react-sdk';
+import { useNavigate } from "react-router-dom";
 
 const SsoSetup = () => {
+  const navigate = useNavigate();
+
   return (
     <div className="data-table-wrapper">
       <div style={{ margin: 'auto', maxWidth: '450px', borderRadius: "10px", overflow: "hidden", width: "100%" }}>
@@ -11,6 +14,7 @@ const SsoSetup = () => {
           flowId="saml-config"
           onSuccess={(e) => {
             console.log('success => ', e)
+            navigate("/");
           }}
           onError={(e) => console.log("Error:")}
         />
