ab62cadb0a9db7cd0c5fbd1b9442b35839d9b700
Chris Carper <chris@descope.com>
2023-08-09 14:46:56 -0500

    reverted small changes in sso setup

--- src/dashboard/ssosetup/SsoSetup.js
+++ src/dashboard/ssosetup/SsoSetup.js
@@ -5,42 +5,21 @@ import { Descope, useSession, useUser, getJwtPermissions, getSessionToken } from
 import { useNavigate } from "react-router-dom";
 
 const SsoSetup = () => {
-  const { isAuthenticated, isSessionLoading } = useSession()
-  const { isUserLoading } = useUser()
-  const permissions = getJwtPermissions(getSessionToken(), 'T2TkjYGrVh6ngFjInykigfBzYQIx')
-  const isSSOAdmin = permissions.indexOf("SSO Admin") > -1 
   const navigate = useNavigate();
 
-  if (!isAuthenticated) {
-    console.log("Redirect to login page");
-    // Handle redirect to login page here
-    return null;
-  }
-
-  if (!isSSOAdmin) {
-    console.log("Redirect to another authenticated page");
-    navigate("/");
-    return null;
-  }
-
   return (
-    <Fragment>
-      { isSSOAdmin &&
-        <div className="data-table-wrapper">
-          <div style={{ margin: 'auto', maxWidth: '450px', borderRadius: "10px", overflow: "hidden", width: "100%" }}>
-            <Descope
-              flowId="saml-config"
-              onSuccess={(e) => {
-                navigate("/");
-                console.log('success => ', e)
-              }}
-              onError={(e) => console.log("Error:", e)}
-            />
-          </div>
-        </div>
-      }
-      {(isSessionLoading || isUserLoading) && <p>Loading...</p>}
-    </Fragment>
+    <div className="data-table-wrapper">
+      <div style={{ margin: 'auto', maxWidth: '450px', borderRadius: "10px", overflow: "hidden", width: "100%" }}>
+        <Descope
+          flowId="saml-config"
+          onSuccess={(e) => {
+            console.log('success => ', e)
+            navigate("/");
+          }}
+          onError={(e) => console.log("Error:")}
+        />
+      </div>
+    </div>
   );
 };
 
