986d5c848688e4ad2487ef82edcace8ed13c403a
Allen Zhou <46854522+allenzhou101@users.noreply.github.com>
2023-08-28 15:59:32 -0500

    Add backend functionality for adding and removing roles

---
+++ api/addRoles.js
@@ -0,0 +1,44 @@
+import DescopeClient from '@descope/node-sdk';
+import * as dotenv from "dotenv";
+
+dotenv.config();
+
+export default async function handler(request, response) {
+    const projectId = request.headers['x-project-id'] || process.env.REACT_APP_DESCOPE_PROJECT_ID;
+
+    // when using cookies
+    // const cookies = request.cookies;
+    // const session_token = cookies.DS; // extract from request. The value is stored typically in DS cookie.
+
+    // when using authorization header
+    const header = request.headers['authorization'];
+    const session_token = header?.split(" ")[1] ?? "";
+
+    const descopeClient = DescopeClient({
+        projectId: projectId,
+        baseUrl: process.env.DESCOPE_BASE_URL
+    });
+
+    try {
+        const jwt = await descopeClient.validateSession(session_token);
+
+        const loginId = jwt.token.loginId;
+        const roleNames = ["Tenant Admin"]
+
+        let resp = await descopeClient.management.user.addRoles(loginId, roleNames)
+
+        response.status(200).json({
+            body: resp,
+            query: request.query,
+            cookies: request.cookies,
+        });
+    } catch (error) {
+        response.status(401).json({
+            body: {},
+            query: request.query,
+            cookies: request.cookies,
+        });
+    }
+    response.send();
+}
+
--- src/components/switches/adminSwitch.js
+++ src/components/switches/adminSwitch.js
@@ -1,3 +1,4 @@
+import { getSessionToken } from '@descope/react-sdk';
 import React, { useState } from 'react';
 import Switch from 'react-switch';
 
@@ -5,13 +6,36 @@ import Switch from 'react-switch';
 export default function AdminSwitch() {
 
     const [checked, setChecked] = useState(null);
+    const [isLoading, setIsLoading] = useState(false);
+    const projectId = localStorage.getItem('projectId') || process.env.REACT_APP_DESCOPE_PROJECT_ID;
+    const sessionToken = getSessionToken();
 
-    const handleChange = (checked) => {
+    const handleChange = async (checked) => {
+        console.log("Toggle");
+        setIsLoading(true);
+        const apiRoute = checked ? "/api/addRoles" : "/api/removeRoles";
+        await fetch(apiRoute, {
+            method: "get",
+            headers: {
+                Accept: "application/json, text/plain, */*",
+                "Content-Type": "application/json",
+                "x-project-id": projectId,
+                Authorization: `Bearer ${sessionToken}`,
+            },
+        })
+            .then((response) => {
+                if (response.status === 401) {
+                } else {
+                    return response.json();
+                }
+            })
+            .catch((err) => console.log('err => ', err));
         if (checked) {
             setChecked(true);
         } else {
             setChecked(false);
         }
+        setIsLoading(false);
     }
 
     return (
@@ -19,7 +43,7 @@ export default function AdminSwitch() {
             <div style={{ marginBottom: "5px" }}>
                 <h4>{checked ? <>Is Admin</> : <>Not Admin</>}</h4>
                 <p style={{ maxWidth: "150px", fontSize: "12px" }}>
-                    Toggle the switch to toggle whether the "Tenant Admin"
+                    Click the switch to toggle whether the "Tenant Admin"
                     role is assigned to your account.
                 </p>
             </div>
