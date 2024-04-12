add6dbd7200105cd20433547d6a0c3c8f78dcd61
Reuven Zabirov <123939006+ruvenzx@users.noreply.github.com>
2024-04-12 12:28:44 -0700

    tenant admin sidebar fix (#206)

--- src/components/sidebar/Sidebar.js
+++ src/components/sidebar/Sidebar.js
@@ -3,7 +3,8 @@ import logo_Dolrr from "../../assets/logo_dolrr.svg";
 import { useNavigate } from "react-router-dom";
 import "./sidebar.scss";
 import { MdBarChart, MdHome, MdKey } from "react-icons/md";
-import { getSessionToken, getJwtRoles } from '@descope/react-sdk'
+import { getSessionToken, getJwtRoles, useUser, useSession } from '@descope/react-sdk'
+
 
 const getItem = (label, key, icon) => {
   return {
@@ -12,26 +13,40 @@ const getItem = (label, key, icon) => {
     label,
   };
 };
+
+
+
 const items = [
-  getItem("Your Rev Dashboard", "/", <MdHome style={{ fontSize: '1.2em' }} />),
+  getItem("Your Rev Dashboard", "/", <MdHome style={{ fontSize: '1.2em'}} />),
   getItem("Admin Dashboard", "/admin/data-tables", <MdBarChart style={{ fontSize: '1.5em' }} />),
   getItem("SSO Setup", "/admin/sso-setup", <MdKey style={{ fontSize: '1.5em' }} />),
   getItem("Management Widgets", "/admin/widgets", <MdKey style={{ fontSize: '1.5em' }} />),
 ];
 
-const getItems = () => {
-  return items.filter(item => item.label !== "Admin Dashboard" || isTenantAdmin());
-}
 
-const isTenantAdmin = () => {
-  const sessionToken = getSessionToken();
-  const roles = getJwtRoles(sessionToken);
-  return roles !== undefined && roles.includes("Tenant Admin");
-}
 
 
 const Sidebar = () => {
   const navigate = useNavigate();
+
+  const { user, isUserLoading } = useUser();
+  const { isSessionLoading } = useSession();
+
+  if (isUserLoading || isSessionLoading) {
+    return null;
+  }
+
+  const isTenantAdmin = () => {
+    if (!user.userTenants) return false;
+    return !!user.userTenants.find(tenant => {
+      return tenant.roleNames.includes("Tenant Admin")
+    })
+  }
+
+  const isTopLevelTenantAdmin = () => {
+    if (!user.roleNames) return false;
+    return user.roleNames.includes("Tenant Admin");
+  }
 
   return (
     <section className="sidebar-container">
@@ -43,7 +58,16 @@ const Sidebar = () => {
         onClick={({ key }) => {
           navigate(key, { state: items.find((elm) => elm.key === key).label });
         }}
-        items={getItems()}
+        items={items.map((item) => {
+          const label = item.label;
+          if (label === "Admin Dashboard" && !isTopLevelTenantAdmin()) {
+            item.disabled = true
+          } 
+          if ((label === "SSO Setup" || label === "Management Widgets") && !isTenantAdmin()) {
+            item.disabled = true;
+          }
+          return item;
+        })}
         selectedKeys={[window.location.pathname]}
       />
     </section>
