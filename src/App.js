0939d947ef0620eab8e3cf65d3dafe8a2d106018
Chris Carper <chris@descope.com>
2024-06-25 12:47:38 -0500

    Add one tap functionality to B2B react sample app

--- src/App.js
+++ src/App.js
@@ -1,22 +1,48 @@
 import Container from "./containers";
-import React from "react";
-import { AuthProvider } from "@descope/react-sdk";
+import React, { useEffect, useCallback } from 'react';
+import { AuthProvider, useDescope, useSession } from '@descope/react-sdk';
 import { useSearchParams } from "react-router-dom";
+let oneTapInitialized = false;
 
 const AppRoot = () => {
   const [searchParams] = useSearchParams();
   const projectId =
-    searchParams.get("project") || localStorage.getItem("projectId");
+    searchParams.get("project") || localStorage.getItem("projectId") || process.env.REACT_APP_DESCOPE_PROJECT_ID;
   if (projectId !== localStorage.getItem("projectId")) {
     localStorage.removeItem("DSR");
     localStorage.removeItem("DS");
     localStorage.setItem("projectId", projectId);
   }
-  // window.analytics.page({ projectId: projectId });
+  
+  const OneTapComp = () => {
+    const sdk = useDescope();
+    const { isAuthenticated, isSessionLoading } = useSession();
+
+    const startOneTap = useCallback(async () => {
+      // eslint-disable-next-line
+      if (oneTapInitialized) return;
+
+      await sdk.fedcm.oneTap('google');
+
+      oneTapInitialized = true;
+    }, [sdk]);
+
+    useEffect(() => {
+      if (!isAuthenticated && !isSessionLoading) {
+        startOneTap();
+      }
+    }, [isAuthenticated, isSessionLoading, startOneTap]);
+
+    // Return some JSX here. For example, return null if there's nothing to render:
+    return null;
+  };
+
   return (
     <AuthProvider
       projectId={projectId || process.env.REACT_APP_DESCOPE_PROJECT_ID}
+      baseUrl="https://api.descope.org"
     >
+      <OneTapComp />
       <App />
     </AuthProvider>
   );
