21fcd97d5b5acc83a644ddcaaffb0a646c41108f
Gilad Shriki <shriki@descope.com>
2024-11-09 21:03:12 -0800

    Add flow param (#253)
    * add flow param
    
    * add flow param
    
    * add flow param

--- src/App.js
+++ src/App.js
@@ -5,14 +5,18 @@ import { useSearchParams } from "react-router-dom";
 
 const AppRoot = () => {
   const [searchParams] = useSearchParams();
-  const projectId =
-    searchParams.get("project") || localStorage.getItem("projectId");
+  const projectId = searchParams.get("project") || localStorage.getItem("projectId");
+  const flowId = searchParams.get("flow") || localStorage.getItem('flowId');
+
   if (projectId !== localStorage.getItem("projectId")) {
     localStorage.removeItem("DSR");
     localStorage.removeItem("DS");
     localStorage.setItem("projectId", projectId);
   }
-  // window.analytics.page({ projectId: projectId });
+  if (flowId !== localStorage.getItem('flowId')) {
+    localStorage.setItem('flowId', flowId);
+  }
+
   return (
     <AuthProvider
       projectId={projectId || process.env.REACT_APP_DESCOPE_PROJECT_ID}
