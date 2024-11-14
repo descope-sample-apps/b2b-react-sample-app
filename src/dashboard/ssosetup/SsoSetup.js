fa4976fc111ecb1e8965a6ed887c2f89a160e245
Reuven Zabirov <ruvenzx0@gmail.com>
2024-11-14 15:51:41 +0200

    fix flowid for sso config panel

--- src/dashboard/ssosetup/SsoSetup.js
+++ src/dashboard/ssosetup/SsoSetup.js
@@ -5,12 +5,13 @@ import { useNavigate } from "react-router-dom";
 
 const SsoSetup = () => {
   const navigate = useNavigate();
-
+  const nonClosable = true;
   return (
     <div className="data-table-wrapper">
       <div style={{ margin: 'auto', maxWidth: '450px', borderRadius: "10px", overflow: "hidden", width: "100%" }}>
         <Descope
-          flowId="saml-config"
+          flowId="sso-config"
+          form={nonClosable}
           onSuccess={(e) => {
             console.log('success => ', e)
             navigate("/");
